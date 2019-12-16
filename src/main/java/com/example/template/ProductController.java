3de8474a72f3c358b972fa70367b65cb220ac807
okhapyok <55180007+okhapyok@users.noreply.github.com>
2019-12-16 16:04:10 +0900

    Update ProductController.java

--- src/main/java/com/example/template/ProductController.java
+++ src/main/java/com/example/template/ProductController.java
@@ -22,7 +22,18 @@ public class ProductController {
 
         return  this.productService.getProductById(productId);
     }
+    @GetMapping("/product/{productId}")
+    Product productStockCheck(@PathVariable(value = "productId") Long productId) {
 
+        System.out.println("productStockCheck call");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return  this.productService.getProductById(productId);
+    }
     @PostMapping("/product")
     Product productInsert(@RequestBody String data) {
         System.out.println(data);
